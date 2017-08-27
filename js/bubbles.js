a531eed700043e19fcde71d4704910414b7525df
Chinmay Pendharkar <notthetup@gmail.com>
2017-08-27 19:53:38 +0800

    tweaked isElementInViewport for edgecases

--- js/bubbles.js
+++ js/bubbles.js
@@ -68,8 +68,8 @@
       return (
         rect.top >= 0 &&
         rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth));
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)+100 &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth))+100;
     }
 
 
