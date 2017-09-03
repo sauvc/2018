64b0019050a231ab2c86a2e16eff24cb9257619e
Chinmay Pendharkar <notthetup@gmail.com>
2017-09-03 15:31:19 +0800

    changed from using webaudio audiosourcenode to mediaelementsource for performance

--- js/bubbles.js
+++ js/bubbles.js
@@ -6,35 +6,36 @@
     const FADE_TIME = 2;
 
     var action = document.getElementById('action');
+    var bubblesaudio = document.getElementById('bubbles-audio');
 
     // WebAudio Stuff
     window.AudioContext = window.AudioContext || window.webkitAudioContext;
     var ac = new AudioContext();
 
     var gain = ac.createGain();
-    var source = ac.createBufferSource();
+    var source = ac.createMediaElementSource(bubblesaudio);
 
     var fadingIn = false;
     var fadingOut = false;
 
-    source.loop = true;
     gain.gain.value = 0.0;
 
     source.connect(gain);
     gain.connect(ac.destination);
+    bubblesaudio.play();
 
-    GetAudioFromURL('../bubbles.mp3', function (err, decodedBuf) {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      source.buffer = decodedBuf;
-      source.start(0);
-      gain.gain.setValueAtTime(0,ac.currentTime);
+    if (isElementInViewport(action)){
+      fadeIn();
+    }
+
+    window.addEventListener('scroll', function() {
       if (isElementInViewport(action)){
         fadeIn();
+      }else{
+        fadeOut();
       }
-    }, null, ac);
+    })
+
 
     function fadeIn(){
       if (!fadingIn){
@@ -43,7 +44,6 @@
         fadingIn = true;
         fadingOut = false;
       }
-
     }
 
     function fadeOut(){
@@ -56,14 +56,6 @@
       }
     }
 
-    window.addEventListener('scroll', function() {
-      if (isElementInViewport(action)){
-        fadeIn();
-      }else{
-        fadeOut();
-      }
-    })
-
     function isElementInViewport (el) {
 
       var rect = el.getBoundingClientRect();
@@ -74,37 +66,6 @@
         (rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)) &&
         (rect.right <= (window.innerWidth || document.documentElement.clientWidth)));
     }
-
-
-    function GetAudioFromURL(URL, onLoadCallback, onProgressCallback, audioContext){
-    if (!audioContext){
-        window.AudioContext = window.AudioContext || window.webkitAudioContext
-        audioContext = new AudioContext();
-    }
-    var request = new XMLHttpRequest();
-    request.open('GET', URL, true);
-    request.responseType = 'arraybuffer';
-    request.onload = function () {
-        audioContext.decodeAudioData(request.response, function(buffer){
-            if (typeof onLoadCallback === 'function')
-                onLoadCallback(null, buffer);
-        },function (){
-            if (typeof onLoadCallback === 'function')
-                onLoadCallback(new Error("Decoding Error"), null);
-        });
-    };
-    request.onerror = function(){
-        if (typeof onLoadCallback === 'function')
-            onLoadCallback(new Error("Loading Error"), null);
-    }
-    request.onprogress = function(event){
-        if (typeof onProgressCallback === 'function'){
-            onProgressCallback(event);
-        }
-    }
-    request.send();
-}
-
 
   })
 }())
